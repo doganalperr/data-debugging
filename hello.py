9430a9975ce48f7fec6949be7943737982441acc
Can Alper <dogancanalper@gmail.com>
2025-12-17 19:39:24 +0300

    Fix full_name formatting and remove breakpoint

--- hello.py
+++ hello.py
@@ -3,18 +3,10 @@
 import sys
 
 def full_name(first_name, last_name):
-    """returns the full name"""
-    name = f"{first_name.capitalize()}{last_name.capitalize()}"
-    
-    return name
+    first = first_name.capitalize() if first_name else ""
+    last = last_name.capitalize() if last_name else ""
 
-if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        # => ['hello.py']
-        print(f'Hello{full_name("", "")}!')
-    elif len(sys.argv) == 2:
-        # => ['hello.py', 'John' ]
-        print(f'Hello {full_name(sys.argv[1], "")}!')
-    else:
-        # => ['hello.py', 'John', 'Lennon']
-        print(f"Hello {full_name(sys.argv[1], sys.argv[2])}!")
+    if first and last:
+        return f"{first} {last}"
+
+    return first or last
